35f392885467d046b3e4da53aa96efa67b691bf0
norm <n.rubin@northeastern.edu>
2024-07-14 09:28:01 -0400

    change revealjs default to no echo input to code blocks

--- fix_meta.py
+++ fix_meta.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from turtle import pos
 from matplotlib.layout_engine import ConstrainedLayoutEngine
+from numpy import fix
 from requests import post
 import yaml
 
@@ -18,6 +19,8 @@ def list_all_files(root_dir):
 
 
 files = os.environ.get('QUARTO_PROJECT_INPUT_FILES', False)
+print(files)
+
 if files:
     files = files.split('\n')
 else:
@@ -45,6 +48,18 @@ def compare_dicts(file, dict1, dict2, path=""):
         current_path = f"{path}.{key}" if path else key
         if key not in dict1:
             print(f"{file}: Key {current_path} found in the second dictionary but not in the first.")
+
+
+def fix_mermaid(lines):
+    i = 0
+    while (i < len(lines)):
+        if lines[i] == '```{mermaid}':
+            if i + 1 == len(lines) or lines[i + 1].strip() != '%%{init: {"flowchart": {"htmlLabels": false}} }%%':
+                    lines.insert(i + 1, '%%{init: {"flowchart": {"htmlLabels": false}} }%%\n')
+                    i += 1 
+        i +=1 
+    return lines
+
 
 
 
@@ -83,6 +98,7 @@ format:
         output-file: xxxx
         scrollable: true
         code-line-numbers: true
+        echo: false 
 sidebar: false 
 execute:
     echo: true
@@ -94,6 +110,7 @@ for file in files:
     directory, _, file_name = file.rpartition('/')
     yaml_template = yaml.safe_load(yaml_without_slides)
     use_revealjs = False 
+
     if directory.endswith('lectures'):
         yaml_template = yaml.safe_load(yaml_with_slides)
         use_revealjs = True
@@ -112,23 +129,30 @@ for file in files:
         if yaml_contents:
             yaml_from_file = yaml.safe_load(yaml_contents)
             # preserve some entries 
-            keeps = ['title', 'tbl-colwidths']
+            keeps = ['title', 'tbl-colwidths', 'echo']
             for keep in keeps:
                 if keep in yaml_from_file:
                     yaml_template[keep] = yaml_from_file[keep]
-
+      
             if use_revealjs:
                 if 'output-location' in yaml_from_file['format']['revealjs']:
                     yaml_template['format']['revealjs']['output-location'] = yaml_from_file['format']['revealjs']['output-location'] 
                 yaml_template['format']['revealjs']['output-file'] =  'revealjs_' + file_name
                 yaml_from_file['format']['revealjs']['output-file'] = 'revealjs_' + file_name
                 yaml_from_file['format']['revealjs']['code-line-numbers'] = True
+                pre = fix_mermaid(pre)
+                post = fix_mermaid(post)
 
+        
             compare_dicts(file, yaml_from_file, yaml_template)
+
+ 
     
             #print(file, yaml_template['title'])
         
 
             new_contents = '\n'.join(pre) + '\n' + yaml.dump(yaml_template) + '\n' + '\n'.join(post)
+
+            break
             with open(file, 'w') as f:
                 f.write(new_contents)
